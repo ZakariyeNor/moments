f6cbf4d83c47a9036c3c118528cdbbf8fe932a9b
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-08-18 15:14:29 +0000

    Add them

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,36 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import stylesa from '../styles/NavBar.module.css'
+import {NavLink} from "react-router-dom"
 
 const NavBar = () => {
     return (
         <Navbar className={stylesa.NavBar} expand="md" fixed='top'>
             <Container>
-                <Navbar.Brand >
-                    <img src={logo} alt='logo' height="45" />
-                </Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt='logo' height="45" />
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link><i className="fa-solid fa-house"></i> Home</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-right-to-bracket"></i> Sign in</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
+                        <NavLink
+                            exact
+                            className={stylesa.NavLink} activeClassName={stylesa.Active}
+                            to="/"><i className="fa-solid fa-house"></i> 
+                            Home
+                        </NavLink>
+                        <NavLink
+                            className={stylesa.NavLink} activeClassName={stylesa.Active}
+                            to="/signin"><i className="fa-solid fa-right-to-bracket">
+                                </i> Sign in
+                        </NavLink>
+                        <NavLink
+                            className={stylesa.NavLink} activeClassName={stylesa.Active}
+                            to="/signup"><i className="fa-solid fa-user-plus">
+                                </i> Sign up
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
